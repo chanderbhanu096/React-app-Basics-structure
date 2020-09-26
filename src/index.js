a481675254a959b9b8c9a5c3048f9f0b75a275be
Chander Bhanu <chanderbhanu096@gmail.com>
2020-09-26 21:53:21 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(
-[<h1>Welcome to React Basics</h1>,<ol><li>Hello</li><li>Hello World</li></ol>],
+  [<h1>Welcome to React Basics</h1>,
+  <ol><li>Hello</li><li>Hello World</li></ol>,
+  <h2>Bye</h2>],
   document.getElementById('root')
 );
 
