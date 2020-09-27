42862f4a6f0ac0da0bca8d7c9f9c98cd6d946d69
Chander Bhanu <chanderbhanu096@gmail.com>
2020-09-27 23:01:27 +0530

    .

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-
+// alternate to <React.Fragment>- </React.Fragment> or [ - ]we can also use <> - </>
 ReactDOM.render(
-  <React.Fragment><h1>Welcome to React Basics</h1>,
-  <ol><li>Hello</li><li>Hello World</li></ol>,
-  <h2>Bye</h2>
-  </React.Fragment>,
+  <>
+    <h1>Welcome to React Basics</h1>,
+    <ol><li>Hello</li><li>Hello World</li></ol>,
+    <h2>Bye</h2>
+  </>,
   document.getElementById('root')
 );
 
