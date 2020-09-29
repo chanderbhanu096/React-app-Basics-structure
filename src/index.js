309461ace0bf653d515ac3256b2b4a2d2c28c8a6
Chander Bhanu <chanderbhanu096@gmail.com>
2020-09-29 17:35:08 +0530

    small change

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import './index.css';
 
-const MyName = "Chander Bhanu's";
-// alternate to <React.Fragment>- </React.Fragment> or [ - ]we can also use <> - </>
-ReactDOM.render(
-  <>
-    <h1>{MyName} Favourite Anime List</h1>
-    <p>List Of Five Best Anime </p>
-    <ol>
-      <li>Fairy Tail</li>
-      <li>The Misfit At the Demon Academy</li>
-      <li>Naruto</li>
-      <li>Attack On Titans</li>
-      <li>Food Wars</li>
-    </ol>
-  </>,
-  document.getElementById('root')
-);
+// const MyName = "Chander Bhanu's";
+// // alternate to <React.Fragment>- </React.Fragment> or [ - ]we can also use <> - </>
+// ReactDOM.render(
+//   <>
+//     <h1>{MyName} Favourite Anime List</h1>
+//     <p>List Of Five Best Anime </p>
+//     <ol>
+//       <li>Fairy Tail</li>
+//       <li>The Misfit At the Demon Academy</li>
+//       <li>Naruto</li>
+//       <li>Attack On Titans</li>
+//       <li>Food Wars</li>
+//     </ol>
+//   </>,
+//   document.getElementById('root')
+// );
 
