e19b08355f0fc0a07110d7044fe82cd396e7a8aa
Chander Bhanu <chanderbhanu096@gmail.com>
2020-10-01 10:34:44 +0530

    current time

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// // import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
 
-// const MyName = "Chander Bhanu's";
-// // alternate to <React.Fragment>- </React.Fragment> or [ - ]we can also use <> - </>
-// ReactDOM.render(
-//   <>
-//     <h1>{MyName} Favourite Anime List</h1>
-//     <p>List Of Five Best Anime </p>
-//     <ol>
-//       <li>Fairy Tail</li>
-//       <li>The Misfit At the Demon Academy</li>
-//       <li>Naruto</li>
-//       <li>Attack On Titans</li>
-//       <li>Food Wars</li>
-//     </ol>
-//   </>,
-//   document.getElementById('root')
-// );
+const MyName = "Chander Bhanu's";
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+// alternate to <React.Fragment>- </React.Fragment> or [ - ]we can also use <> - </>
+ReactDOM.render(
+  <>
+    <h1>{MyName} Favourite Anime List</h1>
+    <p>List Of Five Best Anime </p>
+    <ol>
+      <li>Fairy Tail</li>
+      <li>The Misfit At the Demon Academy</li>
+      <li>Naruto</li>
+      <li>Attack On Titans</li>
+      <li>Food Wars</li>
+    </ol>
+    <h1> Current date :{currentDate}</h1>
+    <h1>current time : {currentTime}</h1>
+  </>,
+  document.getElementById('root')
+);
 
